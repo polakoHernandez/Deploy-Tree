4d817bcea1f3886b8cd4be46bd4ee88a454697f5
polakohernandez <luvaskyhramirez@gmail.com>
2024-02-26 22:18:52 -0500

    Se toma el inputSelectPool.jsx  10:18 a.m  26/02/2024

---
+++ src/Componentes/General/InputSelectPool.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import { Box, TextField, InputLabel } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Autocomplete from "@mui/material/Autocomplete";
+
+function InputSelectPool({
+  size,
+  label,
+  placeholder,
+  icon,
+  name,
+  onChange,
+  options,
+  value,
+  inputRef,
+  disabled,
+}) {
+  const [see, setSee] = useState(false);
+  const theme = createTheme({
+    palette: {
+      luva: {
+        main: "rgb(0,164,228)",
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme} sx={{ position: "relative" }}>
+      <Box sx={{ position: "relative" }}>
+        <InputLabel
+          sx={{
+            position: "absolute",
+            top: "5px",
+            left: { xs: "5%", sm: "5%", md: "5%" },
+            fontFamily: "'Nunito Sans', sans-serif",
+            color: "black",
+          }}
+        >
+          {label}
+        </InputLabel>
+        <Autocomplete
+          size={size}
+          disabled={disabled}
+          name={name}
+          options={options}
+          getOptionLabel={(option) => option.label}
+          onChange={onChange}
+          value={value}
+          ref={inputRef}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              name={name}
+              variant="outlined"
+              focused
+              placeholder={placeholder}
+              color="luva"
+              // value={value}
+              sx={{
+                width: "90%",
+                marginLeft: "5%",
+                marginTop: "30px",
+                borderRadius: "20px",
+                // height: "10px",
+              }}
+              InputProps={{
+                ...params.InputProps,
+                startAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={() => setSee(!see)}
+                      edge="end"
+                      sx={{
+                        backgroundColor: "rgb(0,164,228)",
+                        color: "white",
+                        width: "50px",
+                        height: "41px",
+                        marginTop: "-1px",
+                        marginLeft: "-20px",
+                        marginRight: "10px",
+                        borderRadius: "5px 0px 0px 5px",
+                        "&:hover": {
+                          backgroundColor: "rgb(0,164,228)",
+                        },
+                      }}
+                    >
+                      {icon}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          )}
+        />
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default InputSelectPool;
